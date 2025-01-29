5d28b7bcd2321cc7e7032ef8d05865b0ddf8804c
GregArtHub <153441028+GregArtHub@users.noreply.github.com>
2025-01-29 11:56:07 +0300

    Update para.js

--- para.js
+++ para.js
@@ -1,37 +1,33 @@
-  function generateRandomNumbers() {
-            const numbers = [];
-            for (let i = 0; i < 30; i++) {
-                numbers.push(Math.floor(Math.random() * 100));
+function generateRandomNumbers() {
+    const numbers = [];
+    for (let i = 0; i < 30; i++) {
+        numbers.push(Math.floor(Math.random() * 100));
+    }
+    return numbers;
+}
+
+function fillTable() {
+    const numbers = generateRandomNumbers();
+    let table = document.querySelector('table');
+    let cellIndex = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let row = table.rows[i + 1];
+        for (let j = 0; j < 6; j++) {
+            let cell = row.cells[j];
+            cell.textContent = numbers[cellIndex];
+            if (numbers[cellIndex] >= 50) {
+                cell.classList.add('orange');
+            } else {
+                cell.classList.remove('orange');
             }
-            return numbers;
+            cellIndex++;
         }
+    }
+}
 
-        function fillTable() {
-            const numbers = generateRandomNumbers();
-            let table = document.querySelector('table');
-            let cellIndex = 0;
+function updateNumbers() {
+    fillTable();
+}
 
-            for (let i = 0; i < 5; i++) {
-                let row = table.rows[i + 1];
-                for (let j = 0; j < 6; j++) {
-                    let cell = row.cells[j];
-                    cell.textContent = numbers[cellIndex];
-                    if (numbers[cellIndex] >= 50) {
-                        cell.classList.add('orange');
-                    } else {
-                        cell.classList.remove('orange');
-                    }
-                    cellIndex++;
-                }
-            }
-        }
-
-        function updateNumbers() {
-            fillTable();
-        }
-
-        // Заполняем таблицу при загрузке страницы
-        window.onload = fillTable;
-    </script>
-
-
+window.onload = fillTable;
